ed59c14118fc993149dfb48ccc56eee7fb68d975
Thomas Perl <m@thp.io>
2013-10-17 13:03:03 +0200

    Re-assign well-known TLS slots
    Avoid bionic overwriting existing glibc TLS field values.
    
    Change-Id: I20b88ce74104dbf8e159a9b6d6a42c67f13d391b

--- libc/private/bionic_tls.h
+++ libc/private/bionic_tls.h
@@ -47,12 +47,17 @@ __BEGIN_DECLS
 enum {
   TLS_SLOT_SELF = 0, /* The kernel requires this specific slot for x86. */
   TLS_SLOT_THREAD_ID,
-  TLS_SLOT_ERRNO,
+
+  /**
+   * hybris: TLS "slots" 2-4 need to be unused, as glibc stores some important
+   * data there, stack guard slot is in 5, then just re-assign beyond that
+   **/
+  TLS_SLOT_ERRNO = 6,
 
   /* These two aren't used by bionic itself, but allow the graphics code to
    * access TLS directly rather than using the pthread API. */
-  TLS_SLOT_OPENGL_API = 3,
-  TLS_SLOT_OPENGL = 4,
+  TLS_SLOT_OPENGL_API = 7,
+  TLS_SLOT_OPENGL = 8,
 
   /* This slot is only used to pass information from the dynamic linker to
    * libc.so when the C library is loaded in to memory. The C runtime init
@@ -61,7 +66,7 @@ enum {
   TLS_SLOT_BIONIC_PREINIT = TLS_SLOT_OPENGL_API,
 
   TLS_SLOT_STACK_GUARD = 5, /* GCC requires this specific slot for x86. */
-  TLS_SLOT_DLERROR,
+  TLS_SLOT_DLERROR = 9,
 
   TLS_SLOT_FIRST_USER_SLOT /* Must come last! */
 };
